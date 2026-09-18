f6ee4e246dbf29adcdbc3ba66dd241f55ddcc5c2
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-09-18 18:25:35 +0200

    Auto-sync [2a02-8429-3054-0701-f9f7-5c16-7677-9d92.rev.sfr.net] : 2026-09-18 18:25:35

--- PERIMES/distrobox_local-archive.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Archivage de la distrobox fedora-tools..."
-podman commit fedora-tools fedora-tools:backup
-podman save -o fedora-tools.tar fedora-tools:backup
-echo "Snapshot créé : fedora-tools.tar"
